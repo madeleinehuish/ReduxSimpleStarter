e6a9d2811b80c7604733a1698b767304b909933c
Madeleine Huish <madeleinehuish@gmail.com>
2017-03-16 16:55:28 -0700

    updated searchbar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyDZXHSxdDuQO2r8t53DYmcwzkoS4yaZfoQ';
 
 const App = () => {
-	return <div>Hi!</div>;
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
