5530dc4e6e6965cf4fcde4752989d165b8540603
Madeleine Huish <madeleinehuish@gmail.com>
2017-03-21 08:09:17 -0700

    search bar functionality

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const VideoListItem = ({video}) => {
-	// const video = props.video; is basically what happens when we pull in {video} as props above
+const VideoListItem = ({video, onVideoSelect}) => {
+	// const video = props.video; const onVideoSelect = props.onVideoSelect; is basically what happens when we pull in {video} as props above
 	//it basically takes in the props object, and because there is a props.video it assigns video: props.video
 	const imageUrl = video.snippet.thumbnails.default.url;
 
 	return (
-		<li className="list-group-item">
+		<li onClick={() => onVideoSelect(video)} className="list-group-item">
 			<div className="video-list media">
 				<div className="media-left">
 					<img className="media-object" src={imageUrl} />
